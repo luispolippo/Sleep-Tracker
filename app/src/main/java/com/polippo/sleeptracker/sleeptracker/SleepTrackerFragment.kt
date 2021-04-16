ef0711bb37f472dfea3dbeed63e1ff6a13953636
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-04-16 17:24:05 -0300

    ceated test class and tested the database

--- app/src/main/java/com/polippo/sleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/polippo/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.polippo.sleeptracker.R
+import com.polippo.sleeptracker.database.SleepDatabase
 import com.polippo.sleeptracker.databinding.FragmentSleepTrackerBinding
 
 class SleepTrackerFragment: Fragment() {
@@ -19,6 +21,19 @@ class SleepTrackerFragment: Fragment() {
 
         val binding: FragmentSleepTrackerBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_sleep_tracker, container, false)
+
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        val sleepTrackerViewModel = ViewModelProvider(this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+
+        binding.lifecycleOwner = this
+
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+
         return binding.root
 
     }
